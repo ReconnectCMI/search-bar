a9df23c54e846baa2c1e146e1f74574773f0c7fc
Timon <timonvanwaardhuizen@gmail.com>
2021-06-17 13:22:25 +0200

    who needs prettier anyway?
    we are all pro developers

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { Button, Paper } from '@material-ui/core';
+import React from 'react';
 
 function App() {
   return (
