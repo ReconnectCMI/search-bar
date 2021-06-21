1456de9723d8d9ba7e4e485c4e83269bf4137e59
Timon van Waardhuizen <timonvanwaardhuizen@gmail.com>
2021-06-21 20:16:52 +0200

    Create icon component. (#1)
    Co-authored-by: mio <mio@roxmedia.nl>

---
+++ src/components/icon/icons.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+export default function Icon(props) {
+    const classes = useStyles();
+
+    return (
+        //add the onclick to
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={props.image}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+Icon.propTypes = {
+    image: PropTypes.image,
+    title: PropTypes.string,
+    description: PropTypes.string
+};
