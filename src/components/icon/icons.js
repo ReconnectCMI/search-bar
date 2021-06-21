d87fe406d15973bc052fd0251ce297a9f1ac12bd
miospruit <Mio12@Hotmail.nl>
2021-06-21 23:19:52 +0200

    fix stupid ness

--- src/components/icon/icons.js
+++ src/components/icon/icons.js
@@ -1,33 +1,80 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
+
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
-import TransitionsModal from '../modal/modal';
+
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+import SpeakIcon from '../Speak/SpeakIcon';
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) =>({
     root: {
         maxWidth: 345,
     },
     media: {
         height: 140,
     },
-});
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        width: '80%',
+        height: '70%'
+    },
+}));
 
 export default function Icon(props) {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
     
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     return (
-        
-        //add the onclick to
         <Card className={classes.root}>
-            <TransitionsModal/>
-            <CardActionArea onClick={TransitionsModal.prototype.handleOpen}>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                <div className={classes.paper}>
+                    <h2 id="transition-modal-title">{props.title}</h2>
+                    <p id="transition-modal-description">{props.description}</p>
+                </div>
+                </Fade>
+            </Modal>
+   
+            <CardActionArea onClick={handleOpen}>
                 <CardMedia
                     className={classes.media}
                     image={props.image}
